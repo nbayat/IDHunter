da1242f338a487708cf4f99731bb46b8b0bd7111
Nima Bayat <78981747+nbayat@users.noreply.github.com>
2023-03-07 21:47:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,14 @@ if [ ! -d ~/.bin ]; then
     mkdir -p ~/.bin
 fi
 
+if [ ! -d ./src ]; then
+    curl -fsSL https://raw.githubusercontent.com/nbayat/IDHunter/main/src/IDHunter.c
+fi
+
 # Compile the executable file
 gcc -o ~/.bin/IDHunter src/IDHunter.c
+
+rm ./IDHunter.c
 
 # Add the myapp directory to the PATH environment variable
 echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.zshrc
