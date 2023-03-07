559b7697438e80f6918ea7405a26c34a266c640f
Nima Bayat <78981747+nbayat@users.noreply.github.com>
2023-03-07 21:55:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 # Compile the executable file
 if [ ! -d ./src ]; then
-    curl -o IDHunter.c https://raw.githubusercontent.com/nbayat/IDHunter/main/src/IDHunter.c
+    wait curl -o IDHunter.c https://raw.githubusercontent.com/nbayat/IDHunter/main/src/IDHunter.c
     gcc -o ~/.bin/IDHunter ./IDHunter.c
 else
     gcc -o ~/.bin/IDHunter src/IDHunter.c
